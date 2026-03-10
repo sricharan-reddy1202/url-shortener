2fddf0768000e1830bd4c6d7c1cd76172716993f
Sricharan Reddy <sricharan1202@gmail.com>
2026-03-10 06:37:57 +0530

    Add MongoDB database connection

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express=require("express")
+const connectDB = require("./config/db");
 const app= express();
 const PORT=5000;
+connectDB();
 app.get("/",(req,res)=>{
    res.send("URL Shortener Api running");
 });
