4f300df5e1a648e1f02a5b3f73682b8e297be4f4
Sricharan Reddy <sricharan1202@gmail.com>
2026-03-11 06:04:33 +0530

    Add analytics API for shortened URLs

--- controllers/urlController.js
+++ controllers/urlController.js
@@ -44,3 +44,24 @@ exports.redirectUrl = async (req, res) => {
     res.status(500).json({ error: "Server Error" });
   }
 };
+exports.getAnalytics = async (req, res) => {
+  try {
+
+    const { shortId } = req.params;
+
+    const url = await Url.findOne({ shortId });
+
+    if (!url) {
+      return res.status(404).json({ error: "URL not found" });
+    }
+
+    res.json({
+      originalUrl: url.originalUrl,
+      clicks: url.clicks,
+      createdAt: url.createdAt
+    });
+
+  } catch (error) {
+    res.status(500).json({ error: "Server Error" });
+  }
+};
--- routes/urlRoutes.js
+++ routes/urlRoutes.js
@@ -2,8 +2,9 @@ const express = require("express");
 
 const router = express.Router();
 
-const { createShortUrl, redirectUrl  } = require("../controllers/urlController");
+const { createShortUrl, redirectUrl ,getAnalytics } = require("../controllers/urlController");
 
 router.post("/shorten", createShortUrl);
+router.get("/analytics/:shortId", getAnalytics);
 router.get("/:shortId", redirectUrl);
 module.exports = router;
